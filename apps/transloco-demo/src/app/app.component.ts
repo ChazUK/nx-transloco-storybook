b432170bf8d21f7bd3e463227bcc2935fd904409
Charlie Francis <charliefrancis@gmail.com>
2020-07-10 18:11:37 +0100

    initial commit

---
+++ apps/transloco-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-transloco-storybook-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'transloco-demo';
+}
