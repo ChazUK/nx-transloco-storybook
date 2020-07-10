2ebb8a273a2bc75dff41f930a0f41b48df87a8cd
Charlie Francis <charliefrancis@gmail.com>
2020-07-10 19:12:11 +0100

    Modifying Storybook with Transloco config

--- libs/ui/.storybook/config.js
+++ libs/ui/.storybook/config.js
@@ -1,5 +1,27 @@
-import { configure, addDecorator } from '@storybook/angular';
+// import {
+//   translocoConfig,
+//   TranslocoModule,
+//   TRANSLOCO_CONFIG,
+// } from '@ngneat/transloco';
 import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, configure, moduleMetadata } from '@storybook/angular';
+
+addDecorator(
+  moduleMetadata({
+    //   imports: [TranslocoModule],
+    //   providers: [
+    //     {
+    //       provide: TRANSLOCO_CONFIG,
+    //       useValue: translocoConfig({
+    //         reRenderOnLangChange: true,
+    //         availableLangs: ['en', 'de'],
+    //         defaultLang: 'en',
+    //         prodMode: false,
+    //       }),
+    //     },
+    //   ],
+  })
+);
 
 addDecorator(withKnobs);
 configure(require.context('../src/lib', true, /\.stories\.(j|t)sx?$/), module);
